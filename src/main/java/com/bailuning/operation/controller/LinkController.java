ceebc02824ac2fc14e15f78143bd18dc55b73015
白鲁宁 <932030467@qq.com>
2019-03-23 16:03:55 +0800

    短链接转换

--- src/main/java/com/bailuning/operation/controller/LinkController.java
+++ src/main/java/com/bailuning/operation/controller/LinkController.java
@@ -10,12 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author bailuning
